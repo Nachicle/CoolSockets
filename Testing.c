28c5737e4c64841daa93c706cab71812f3b6c6d9
Nachicle <hi@nachicle.com>
2022-06-20 23:17:15 +0200

    Adding Mac sources

---
+++ Testing.c
@@ -0,0 +1,7 @@
+#include <CoolSockets.h>
+
+int main(int argc, char const *argv[])
+{
+    CS_Version();
+    return 0;
+}
---
+++ source/mac/CoolSockets.c
@@ -0,0 +1,8 @@
+ #include <CoolSockets.h>
+
+#include <stdio.h>
+
+// Library version function
+void CS_Version(void) {
+    printf("Hi, I'm CoolSockets (%s) for Mac!\n", COOL_SOCKETS_VERSION);
+}
